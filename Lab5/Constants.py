c96ed6a09cbe6d0e73f13b02ae5c2f5b67bb9ba4
Nillado <Nillado@COMPUTER>
2020-10-06 17:40:55 +0300

    ещё корректировка

--- Lab5/Constants.py
+++ Lab5/Constants.py
@@ -2,11 +2,13 @@ import numpy
 import math
 from MathCounting import *
 
+N = 9
+
+trainingRate = 0.2
+
 X = numpy.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 Y = numpy.array([3.6, 4.4, 5.8, 6.2, 7.4, 8, 9.2, 10.4, 11.8])
-
-N = 9
 
 E = numpy.zeros(N)
 
@@ -17,6 +19,8 @@ sumY = Y.sum()
 sumXY = (X*Y).sum()
 
 sumXX = (X*X).sum()
+
+
 
 
 
